7aad541278f617750731fee78717351503fb8945
George McMullen <gmcmullen@mail.greenriver.edu>
2021-01-21 14:32:46 -0500

    Added controller

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /', function () {
+    echo "Hello world";
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
